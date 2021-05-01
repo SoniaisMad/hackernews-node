bf69666c9c55414f2f68b04f4360170f7f10f079
SoniaIsMad <manoubisonia@gmail.com>
2021-05-01 17:27:14 +0200

    authentication part

--- src/index.js
+++ src/index.js
@@ -2,62 +2,19 @@ const { ApolloServer } = require('apollo-server');
 const fs = require('fs');
 const path = require('path');
 const { PrismaClient } = require('@prisma/client');
+const { getUserId } = require('./utils');
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const prisma = new PrismaClient();
 // 2 - resolvers objects is the actual implementation of the schema
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: async (parent, args, context, info) => {
-      return context.prisma.link.findMany()
-    },
-    link: (parent, args, context) => {
-      const result = context.prisma.link.findUnique({
-        where: {
-          url: args.url
-        }
-      })
-      return result
-    }
-  },
-  Mutation: {
-    post: (parent, args, context) => {
-      const newLink = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description
-        }
-      })
-      return newLink
-    },
-    deleteLink: (parent, args, context) => {
-      const id = +args.id;
-      const deletedLink = context.prisma.link.delete({
-        where: {
-          id
-        }
-      })
-      return deletedLink
-    },
-    updateLink : (parent, args, context) => {
-      const id = +args.id;
-      const updatedLink = context.prisma.link.update({
-        where: {
-          id
-        },
-        data : {
-          url: args.url,
-          description: args.description
-        }
-      })
-      return updatedLink
-    }
-  },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 // 3 - schema and resolvers are passed to the ApolloServer
@@ -67,10 +24,17 @@ const server = new ApolloServer({
     'utf8'
   ),
   resolvers,
-  context: {
-    prisma
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
   }
-})
+});
 
 server
   .listen()
---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,102 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
+async function post(parent, args, context, info) {
+    const { userId } = context;
+
+    if (userId === null) {
+      throw new Error('User Not authenticated');
+    }
+  
+    return await context.prisma.link.create({
+      data: {
+        url: args.url,
+        description: args.description,
+        postedBy: { connect: { id: userId } },
+      }
+    })
+}
+
+async function deleteLink(parent, args, context){
+  const { userId } = context;
+
+    if (userId === null) {
+      throw new Error('User Not authenticated');
+    }
+  
+    const id = +args.id;
+    const deletedLink = context.prisma.link.delete({
+        where: {
+        id
+        }
+    })
+    return deletedLink
+}
+
+async function updateLink(parent, args, context){
+  const { userId } = context;
+
+  if (userId === null) {
+    throw new Error('User Not authenticated');
+  }
+
+    const id = +args.id;
+    const updatedLink = context.prisma.link.update({
+        where: {
+        id
+        },
+        data : {
+        url: args.url,
+        description: args.description
+        }
+    })
+    return updatedLink
+}
+
+async function signup(parent, args, context, info) {
+    // 1
+    const password = await bcrypt.hash(args.password, 10)
+  
+    // 2
+    const user = await context.prisma.user.create({ data: { ...args, password } })
+  
+    // 3
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  
+    // 4
+    return {
+      token,
+      user,
+    }
+  }
+  
+  async function login(parent, args, context, info) {
+    // 1
+    const user = await context.prisma.user.findUnique({ where: { email: args.email } })
+    if (!user) {
+      throw new Error('No such user found')
+    }
+  
+    // 2
+    const valid = await bcrypt.compare(args.password, user.password)
+    if (!valid) {
+      throw new Error('Invalid password')
+    }
+  
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  
+    // 3
+    return {
+      token,
+      user,
+    }
+  }
+  
+  module.exports = {
+    signup,
+    login,
+    post,
+    updateLink,
+    deleteLink
+  }
